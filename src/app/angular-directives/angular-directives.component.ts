938e24e6832a38b4f587dc4880210fef3d79d643
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-09-22 18:21:38 +0200

    try out *ngIf  & hidden

--- src/app/angular-directives/angular-directives.component.ts
+++ src/app/angular-directives/angular-directives.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class AngularDirectivesComponent implements OnInit {
   nums: Array<number>
 
+  addNewNum(): void {
+    this.nums.push(1)
+  }
+
   constructor() {
     this.nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   }
